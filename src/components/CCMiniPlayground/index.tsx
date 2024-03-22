bbbf04a1aaa82cb365631f822210c99ef5deee26
老王天天写bug <wlwzh9927@163.com>
2024-03-22 17:10:31 +0800

    ✨ feat(cc playground):
    change sample img to DST game

--- src/components/CCMiniPlayground/index.tsx
+++ src/components/CCMiniPlayground/index.tsx
@@ -1,38 +1,43 @@
-import { Alignment, H5, Menu, MenuDivider, NavbarDivider, Slider, Switch } from '@blueprintjs/core'
+import { H5, NavbarDivider, Slider, Switch } from '@blueprintjs/core'
 import * as PIXI from 'pixi.js'
 import { MIPMAP_MODES, SCALE_MODES } from 'pixi.js'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import shader from './shader'
+// @ts-ignore
+import sample_img from "./sample_image.jpg"
 
-interface IProps {
+type CCMiniPlaygroundProps = {
   cc: string,
 }
 
-interface AppUniforms {
+type AppUniforms = {
   percent: number,
 }
 
-export default function CCMiniPlayground(props: IProps) {
+export default function CCMiniPlayground(props: CCMiniPlaygroundProps) {
   const {cc} = props
 
   const [percent, SetPercent] = useState(100)
   const [enabled, setEnabled] = useState(true)
   const shaderPercent = enabled ? percent / 100 : 0
   const ref = useRef<HTMLDivElement>()
-  const appUniforms = useRef<AppUniforms>({percent: 1.0}).current
+  const appUniforms = useRef<AppUniforms>({percent: 1.0})
 
   useEffect(()=> {
-    appUniforms.percent = shaderPercent
+    appUniforms.current.percent = shaderPercent
   }, [shaderPercent])
 
   useEffect(()=> {
     if (ref.current) {
       ref.current.innerHTML = ""
 
-      const app = new PIXI.Application({width: 625, height: 285}) // TODO: 分辨率?
+      // const img = PIXI.Sprite.from("https://pic.imgdb.cn/item/6510184ec458853aef81aff4.png") //  TODO: 图片来源？
+      const img = PIXI.Sprite.from(sample_img)
+      const {width, height} = img
+      
+      const app = new PIXI.Application({width: width / 4, height: height / 4})
+      app.view.style.width = "100%"
       ref.current.appendChild(app.view as any)
-
-      const img = PIXI.Sprite.from("https://pic.imgdb.cn/item/6510184ec458853aef81aff4.png") //  TODO: 图片来源？
 
       img.width = app.screen.width
       img.height = app.screen.height
@@ -45,7 +50,7 @@ export default function CCMiniPlayground(props: IProps) {
       img.filters = [filter]
 
       app.ticker.add(() => {
-        filter.uniforms.percent = appUniforms.percent
+        filter.uniforms.percent = appUniforms.current.percent
       })
 
       return ()=> app.destroy()
@@ -55,13 +60,12 @@ export default function CCMiniPlayground(props: IProps) {
   return (
     <div key={cc} style={{userSelect: "none", WebkitUserSelect: "none"}}>
       <H5>滤镜预览</H5>
-      {/* <p>这是一个<a onClick={()=> alert("还没写完")}>色彩映射滤镜</a>，你可以感受一下它的风格。</p> */}
       <div style={{width: 70, userSelect: "none", display: "flex"}}>
         <Switch 
           style={{marginRight: 0, transform: "translateY(-4px)"}}
-          checked={enabled} onChange={(e)=> 
-            //@ts-ignore
-            setEnabled(e.target.checked)}/>
+          checked={enabled} 
+          //@ts-ignore
+          onChange={(e)=> setEnabled(e.target.checked)}/>
         <NavbarDivider style={{marginRight: 20}}/>
         <Slider 
           min={0} max={100} 
@@ -69,7 +73,7 @@ export default function CCMiniPlayground(props: IProps) {
           disabled={!enabled}
           value={percent} onChange={v=> SetPercent(v)}/>
       </div>
-      <div ref={ref} style={{zoom: "45%"}}/>
+      <div ref={ref} style={{maxWidth: 800}}/>
     </div>
   )
 }
